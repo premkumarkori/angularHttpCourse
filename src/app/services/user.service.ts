670ede3e038044b1e055bf372c1a34edf21c292f
premkumarkori <premkumar.kori@mavs.uta.edu>
2022-07-03 23:39:14 -0500

    Third Commit: Use of RxJS Operators

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,6 +1,8 @@
  import { Injectable } from '@angular/core';
  import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable,} from 'rxjs';
+import { map,retry,tap } from 'rxjs/operators';
+
 import { User } from '../interfaces/user';
 import { environment } from 'src/environments/environment';
 @Injectable({
@@ -9,35 +11,68 @@ import { environment } from 'src/environments/environment';
 export class UserService {
   private apiUrl = environment.apiUrl;
   readonly moreParams = ['test1', 'test2'];
+  readonly defaultImage = 'https://robohash.org';
 
   constructor(private http: HttpClient) {}
 
+  // getUsers(): Observable<User[]> {
+  //   //HttpHeaders example
+  //   // let  myHeaders = new HttpHeaders({'myHeaders':['myValue','myValue2']});
+  //   // myHeaders = myHeaders.set('id', '1234');
+  //   // myHeaders = myHeaders.append('id', '100');
+
+  //   //HttpParams example 1
+  //   // let myParams = new HttpParams().set('page',1).set('size', '5');
+  //   // myParams = myParams.set('name', 'premkumar');
+  //   // myParams = myParams.append('name', 'kori');
+
+  //   //HttpParams example 2
+  //   // const theParams = {['testList']: this.moreParams}
+  //   // let myParams = new HttpParams({fromObject: theParams});
+
+  //   //HttpParams example 3
+  //   // let myParams = new HttpParams({fromString: 'page=1&size=7'});
+  //   // return this.http.get<User[]>(`${this.apiUrl}/users`,{params:myParams});
+
+  //   return this.http.get<User[]>(`${this.apiUrl}/users`);
+  // }
+  // getUsers(): Observable<User[]> {
+  //   return this.http.get<User[]>(`${this.apiUrl}/users`)
+  //   .pipe(
+  //     //tap(users =>console.log(users)),
+  //     map(users => users.map(user => ({
+  //       email: user.email,
+  //       website: user.website,
+  //       phone: user.phone,
+  //       username: user.username,
+  //       name: user.name.toUpperCase(),
+  //       image: `${this.defaultImage}/${user.username.toLowerCase()}`,
+  //       isAdmin: user.id === 10? 'admin' :'user'
+  //     })))
+  //   );
+  // }
+
+  // getUsers(): Observable<User[]> {
+  //   return this.http.get<User[]>(`${this.apiUrl}/users1111`)
+  //   .pipe(
+  //     retry(3)
+  //   );
+  // }
+
   getUsers(): Observable<User[]> {
-    //HttpHeaders example
-    // let  myHeaders = new HttpHeaders({'myHeaders':['myValue','myValue2']});
-    // myHeaders = myHeaders.set('id', '1234');
-    // myHeaders = myHeaders.append('id', '100');
-
-    //HttpParams example 1
-    // let myParams = new HttpParams().set('page',1).set('size', '5');
-    // myParams = myParams.set('name', 'premkumar');
-    // myParams = myParams.append('name', 'kori');
-
-    //HttpParams example 2
-    // const theParams = {['testList']: this.moreParams}
-    // let myParams = new HttpParams({fromObject: theParams});
-
-    //HttpParams example 3
-    // let myParams = new HttpParams({fromString: 'page=1&size=7'});
-    // return this.http.get<User[]>(`${this.apiUrl}/users`,{params:myParams});
-
     return this.http.get<User[]>(`${this.apiUrl}/users`);
   }
+
   getTextFile(): Observable<string> {
     return this.http.get(`assets/text.txt`, {responseType: 'text'});
   }
   getUser(): Observable<User> {
-    return this.http.get<User>(`${this.apiUrl}/users/1`);
+    return this.http.get<User>(`${this.apiUrl}/users/1`)
+    .pipe(
+      map(user => {
+        return {...user,isAdmin: true, searchKey: [user.name,user.username]}
+      })
+    );
   }
 
   createUser(user: User): Observable<User> {
